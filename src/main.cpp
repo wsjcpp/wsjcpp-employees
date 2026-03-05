f009f46c942b187e12b1af26d6c4b2872b20a50e
Evgenii Sopov <mrseakg@gmail.com>
2026-03-06 00:03:26 +0700

    Added/Updated license info source code files/LICENSE

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************************
- * Copyright (c) 2020-2025 Evgenii Sopov <mrseakg@gmail.com>
+ * Copyright (c) 2020-2026 Evgenii Sopov <mrseakg@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,8 @@
  * SOFTWARE.
  *
  ***********************************************************************************/
+
+// Original repository: https://github.com/wsjcpp/wsjcpp-employees
 
 #include "my_impl.h"
 #include <wsjcpp_core.h>
